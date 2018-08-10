08f934ee87981e6c3f1dbf26c95d78f3c887f3c8
Steven Penava <steven.penava@gmail.com>
2018-08-10 10:32:38 -0400

    TASK#3: Added logic for saving email, password, and remember me checkbox.

--- src/pages/SignupLogin.js
+++ src/pages/SignupLogin.js
@@ -16,22 +16,68 @@ import '../styles/SignupLogin.css';
 *  @Description: Signup/Login page for users of the site
 /*/
 class SignupLogin extends Component {
+	constructor(props) {
+		super(props);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+		this.handleSubmission = this.handleSubmission.bind(this);
+		this.handleRememberMeChange = this.handleRememberMeChange.bind(this);
+
+		this.state = {
+			email: "",
+			password: "",
+			rememberMeIsChecked: true
+		};
+	}
+
+	handleEmailChange(event) {
+		this.setState({email: event.target.value});
+	}
+
+	handlePasswordChange(event) {
+		this.setState({password: event.target.value});
+	}
+
+	handleRememberMeChange(event) {
+		this.setState(
+			{rememberMeIsChecked: !this.state.rememberMeIsChecked}, () => {
+				console.log("Checked: " + this.state.rememberMeIsChecked);
+			}
+		);
+		
+	}
+
+	handleSubmission(event) {
+		console.log("Email: " + this.state.email);
+		console.log("Password: " + this.state.password);
+		console.log("Remember: " + this.state.rememberMeIsChecked);
+
+		alert("Email, password, remember me status: "  + this.state.email + ", " + this.state.password + ", " + this.state.rememberMeIsChecked);
+		event.preventDefault();
+	}
+
 	render(){
 		return(
 			<div className="login-box">
 				<form class="form-signin">
 					<img class="mb-4" src="https://s8.postimg.cc/qh5ck71gl/test_logo.png" alt="" width="102" height="102" />
+					
 					<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+					
 					<label for="inputEmail" class="sr-only">Email address</label>
-					<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" />
+					<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autoFocus="" value={this.state.email} onChange={this.handleEmailChange} />
+					
 					<label for="inputPassword" class="sr-only">Password</label>
-					<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" />
+					<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" value={this.state.password} onChange={this.handlePasswordChange} />
+					
 					<div class="checkbox mb-3">
-					<label>
-						<input type="checkbox" value="remember-me" /> Remember me
-					</label>
+						<label>
+							<input type="checkbox" value="remember-me" onChange={this.handleRememberMeChange} checked={this.state.rememberMeIsChecked} /> Remember me
+						</label>
 					</div>
-					<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+					
+					<button class="btn btn-lg btn-primary btn-block" type="submit" onClick={this.handleSubmission}>Sign in</button>
+					
 					<p class="mt-5 mb-3 text-muted">© 2018</p>
 				</form>
 			</div>
